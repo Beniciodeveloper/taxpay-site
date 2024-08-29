6070edb800a594cc075a7d478dfbf7116ed1b4a6
Lucas <benicio.developer@gmail.com>
2024-08-29 20:33:05 -0300

    inplemeting team

--- pages/equipe.vue
+++ pages/equipe.vue
@@ -1,8 +1,11 @@
 <template>
   <section class="team-page">
-    <div class="container">
+        <div>
+
+          
       <h2>Time</h2>
-      <div v-for="item in teams" :key="item.name">
+      <div class="team-page__members ">
+        <div class="team-page__cards" v-for="item in teams" :key="item.name">
         <team-member
           :name="item.name"
           :role="item.role"
@@ -10,12 +13,17 @@
         >
         </team-member>
       </div>
+      </div>
+      
     </div>
+
   </section>
 </template>
 
 <script>
+
 import TeamMember from "~/components/team-member.vue";
+
 
 export default {
   components: {
@@ -70,4 +78,7 @@ export default {
 };
 </script>
 
-<style></style>
+
+<style lang="sass">
+@import "@/assets/styles/pages/equipe"
+</style>
